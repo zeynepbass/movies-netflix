df757cb5fdfede22747e1c3e749b6dcf9e430249
Zeynep <101248567+zeynepbass@users.noreply.github.com>
2024-01-10 14:32:10 +0300

    sdfsdf
    sfdsfd

--- frontend/src/Pages/Error.jsx
+++ frontend/src/Pages/Error.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Error = () => {
   return (
     <>
-        <img src="/images/40.webp" style={{maxWidth:"100%",height:"100vh"}} alt=""></img>
+        <img src="/images/404.webp" style={{maxWidth:"100%",height:"100vh"}} alt=""></img>
     </>
   )
 }
